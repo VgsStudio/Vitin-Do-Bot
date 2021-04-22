98c08e779ebdcfadfce1c5d77c35fa9cdd2b0e32
Vitor Soller <vgsoller@gmail.com>
2021-04-21 23:30:40 -0300

    Resvolvendo Bugs

--- lari/func_lari.py
+++ lari/func_lari.py
@@ -4,10 +4,15 @@ from os import environ
 
 from func_global import *
 
-consumer_key = environ['consumer_key']
+"""consumer_key = environ['consumer_key']
 consumer_secret = environ['consumer_secret']
 access_token = environ['access_token']
 access_token_secret = environ['access_token_secret']
+"""
+consumer_key = 'E8unZOskXX4fRJkEt7HizREwS'
+consumer_secret = 'QD0bwerax4gJp2F7GVbukBzqrGDueDTb9WhhezBJQz0TjH7lRI'
+access_token = '1369432777742049285-XLgRqC0F3aGsHH8veSLI4qxNAVemLt'
+access_token_secret = 'vC9ddfcbKj4XLfHDvRddlVPy7hrVKJeyaQAbXDVR3ZxDo'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
